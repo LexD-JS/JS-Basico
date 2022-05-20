dcad8123a3dfdf9587cefe6fd878efb87977bb10
Alejandro <alejandrodiazortega@hotmail.com>
2022-05-19 21:32:52 -0500

    Valores verdaderos y falsos

---
+++ Truthy&Falsy.js
@@ -0,0 +1,19 @@
+// Truthy son todos aquellos valores que tengan un valor Boolean como verdadero o falso
+
+//Verdaderos
+"hola"
+" "//Un string con un espacio en
+30
+{a:1}
+[1,3]
+true
+function a(){} //Todas las funciones son verdaderas
+
+
+"" // un string vacío 
+0 -0
+null
+NaN
+false
+undefined
+
