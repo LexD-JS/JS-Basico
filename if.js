967d518219af2f695ef2a5f71c5e68b69693d74f
Alejandro <alejandrodiazortega@hotmail.com>
2022-05-20 02:11:15 -0500

    If statement

---
+++ if.js
@@ -0,0 +1,19 @@
+/*Imaginemos que le pedimos permiso a nuestros padres para salir de fiesta, pero para que nos den permiso
+primero tenemos que cumplir con nuestra tarea*/
+
+
+let tarea=true
+
+if( tarea == true ){
+    console.log("Puedes ir a divertirte :)");
+}
+else{
+    console.log("No puedes ir, no has terminado tus deberes :c")
+}
+
+
+//El if se ejecuta solo y solo si la condicion a evaluar es verdadera.
+//Se puede anidar (unir) más de un if... esto es sumamente útil cuando tenemos 
+//dos o más cituaciones por validar.
+
+//En el caso que la condición NO se cumpla, simplemente se ignorará la sentencia y se ejecutará el else.
